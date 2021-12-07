27c9dc68be09c9c319e5b5de86f2b48e4291a0e1
Souryendu Das <bubunda@tamu.edu>
2021-12-07 14:16:42 -0600

    Removed access token from code, added it as an argument. It's a security flaw

--- jobs4me/notifications/send_notif.py
+++ jobs4me/notifications/send_notif.py
@@ -5,10 +5,10 @@ from pywebio.session import *
 import time
 import sys
 
-access_token = "o.pdFGK55yoK9TZwofCpInnwCotGp4BgGy"
-data = "Hi " + str(sys.argv[1]) + " "
+access_token = sys.argv[1]
+data = "Hi " + str(sys.argv[2]) + " "
 text = "The Message is "
-for i in range(2, len(sys.argv)):
+for i in range(3, len(sys.argv)):
     text = text + " " + str(sys.argv[i])
 pb = PushBullet(access_token)
 push = pb.push_note(data, text)
