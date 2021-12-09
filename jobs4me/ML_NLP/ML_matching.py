dc76bf71eddfdd93a3143eaefd5d6282bdc586c3
Dean Orenstein <dean.orenstein2@gmail.com>
2021-12-09 15:59:30 -0600

    Fix nan issue (actually)

--- jobs4me/ML_NLP/ML_matching.py
+++ jobs4me/ML_NLP/ML_matching.py
@@ -39,7 +39,7 @@ def getSuitableJobs(resume_list, username):
                 file_location = 'jobs4me/user_csvs/user_' + username + '/resumes/' + str(column)
                 resume = extract_text(file_location)
                 text_resume = str(resume)
-                if not np.isnan(comments[comment_idx]):
+                if not type(comments[comment_idx]) == float:
                     text_resume += " " + comments[comment_idx]
                 summarize(text_resume, ratio=1)
             if str(column).find("@") != -1:
