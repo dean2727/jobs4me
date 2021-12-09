36d04d6c865931fa80b3c4f749b9d99e5e78f01c
Dean Orenstein <dean.orenstein2@gmail.com>
2021-12-09 15:25:52 -0600

    Open job posting in new tab, fix nan cell issue

--- jobs4me/ML_NLP/ML_matching.py
+++ jobs4me/ML_NLP/ML_matching.py
@@ -38,7 +38,9 @@ def getSuitableJobs(resume_list, username):
             if str(column).find(".pdf") != -1:
                 file_location = 'jobs4me/user_csvs/user_' + username + '/resumes/' + str(column)
                 resume = extract_text(file_location)
-                text_resume = str(resume) + " " + comments[comment_idx]
+                text_resume = str(resume)
+                if not np.isnan(comments[comment_idx]):
+                    text_resume += " " + comments[comment_idx]
                 summarize(text_resume, ratio=1)
             if str(column).find("@") != -1:
                 email = str(column)
